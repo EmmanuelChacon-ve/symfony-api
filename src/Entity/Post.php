bd107c180e0b1437141dfec99f1df8b202e0f016
Emmanuel Chacon <emmanuelechra@gmail.com>
2023-12-19 19:51:43 -0400

    commit

---
+++ src/Entity/Post.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post as Store;
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert; //para validar
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            normalizationContext: ['groups' => ['read', 'read:item']],
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => ['read', 'read:collection']],
+        ),
+        new Patch(),
+        new Store(),
+    ],
+    // normalizationContext: ['groups' => ['read']], // GET
+    denormalizationContext: ['groups' => ['write']],
+     // POST, PUT, PATCH
+    paginationItemsPerPage: 8,
+)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['read', 'write'])]
+    #[Assert\NotBlank] //no permite espacios en blanco validacion
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['read:item', 'write'])]
+    #[Assert\NotBlank]
+    private ?string $body = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['read', 'write'])]
+    #[Assert\NotBlank]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    #[Groups(['read:collection'])]
+    public function getSummary($len = 70): ?string
+    {
+        if (mb_strlen($this->body) <= $len) {
+            return $this->body;
+        }
+
+        return mb_substr($this->body, 0, $len) . '[...]';
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
